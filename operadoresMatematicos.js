e02adb7b23c258dcb6da479c9f95c687c37824eb
Joel-A-Queiroz <joel.andradequeiroz2018@gmail.com>
2026-03-13 14:57:27 -0300

    Adiciona mais operadores

--- operadoresMatematicos.js
+++ operadoresMatematicos.js
@@ -1,9 +1,9 @@
-const idadeJoel = 28
-const idadePedro = 23
+//const idadeJoel = 28
+//const idadePedro = 23
 
 
 //comparador de igualdade:
-const comparacao = idadeJoel != idadePedro
+//const comparacao = idadeJoel != idadePedro
 
 
 //Maior que:
@@ -13,25 +13,47 @@ const comparacao = idadeJoel != idadePedro
 //Menor que:
 //const comparacao = idadeJoel < idadePedro
 
-console.log('São diferentes? ', comparacao);
+//console.log('São diferentes? ', comparacao);
 
 
 //Comparação de idade:
-const idade = 29
-const idadeIrmao = 18
+//const idade = 29
+//const idadeIrmao = 18
 
-const souMaiorDeIdade = idade >= 18
-const irmaoMarioDeIdade = idadeIrmao >= 18
+//const souMaiorDeIdade = idade >= 18
+//const irmaoMarioDeIdade = idadeIrmao >= 18
 
-console.log('Sou maior de idade? ', souMaiorDeIdade)
-console.log('Irmão maior de idade? ', irmaoMarioDeIdade)
+//console.log('Sou maior de idade? ', souMaiorDeIdade)
+//console.log('Irmão maior de idade? ', irmaoMarioDeIdade)
 
 
 //comparador estrito:
 
-const idadeAtual = 28
-const idadeOutu = '28'
+//const idadeAtual = 28
+//const idadeOutu = '28'
 
-const estritamente = idadeAtual === idadeOutu
-console.log('Minha idade atual é a mesma que em outubro? ', estritamente);
+//const estritamente = idadeAtual === idadeOutu
+//console.log('Minha idade atual é a mesma que em outubro? ', estritamente);
+
+
+
+//Combinando comparadores:
+
+const idade = 17
+const maiorDeIdade = idade >=18
+const possuiCNH = false
+
+const podeDirigir = maiorDeIdade && possuiCNH
+
+console.log("Pode Digirir? ", podeDirigir)
+
+const podeViajarSozinha = maiorDeIdade || possuiCNH;
+
+console.log('Pode viajar sozinha? ', podeViajarSozinha)
+
+const precisaDeAcompanhante = !maiorDeIdade
+
+console.log('Precisa de Acompanhante? ', precisaDeAcompanhante);
+
+
 
