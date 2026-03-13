b7a9a8021ffa23ac066aed31aaf1305bae2b2d3c
Joel-A-Queiroz <joel.andradequeiroz2018@gmail.com>
2026-03-13 19:34:14 -0300

    Terminando desafio.js

---
+++ desafio.js
@@ -0,0 +1,70 @@
+//console.log('Boas vindas a calculadora de troco')
+
+//let valorCompra = 17.50
+//let valorPago = 20
+
+//console.log('Valor pago: R$ ', valorPago)
+//console.log('Valor compra: R$ ', valorCompra)
+
+//let valorTroco = valorPago - valorCompra
+
+//console.log('Valor do troco é: R$', valorTroco)
+
+// Verificação de maioridade:
+
+let idade = 18
+let maiorDeIdade = idade >= 18
+
+
+console.log("É maior de idade? ", maiorDeIdade);
+
+// Verificação de nota:
+
+let portugues = 10
+let matematica = 4
+let media = (portugues + matematica)/2
+
+console.log('Média: ', media);
+
+let passouDeAno = media >=7
+console.log('Passou de ano? ', passouDeAno);
+
+//Simulação troco:
+
+let valorCompra = 35.90
+let valorPago = 50
+
+console.log('Valor compra:R$ ', valorCompra)
+console.log('Valor Pago: R$ ', valorPago)
+
+let valorTroco = valorPago - valorCompra
+
+console.log('Valor do troco é: R$ ', valorTroco)
+
+// Verificação de senha:
+
+const senhaPrimeiro = 1234
+const senhaSegundo = 1233
+
+console.log('A senha é igual?: ', senhaPrimeiro == senhaSegundo);
+
+//Validação de faltas:
+
+let totalAulas = 100
+let totalFaltas = 24
+
+let porcentagemDefaltas = totalFaltas / totalAulas *100
+
+console.log('Porcentagem: ', porcentagemDefaltas)
+
+let aprovado = porcentagemDefaltas < 25
+
+console.log('Aprovado?: ', aprovado);
+
+//Verificação de login:
+
+let temLogin = true
+let temSenha = true
+
+console.log('Tem login e senha? ', temLogin && temSenha);
+
