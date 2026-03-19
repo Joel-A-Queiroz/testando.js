757dcc628cf95ca28b5b52a67cbc389892ea0d24
Joel-A-Queiroz <joel.andradequeiroz2018@gmail.com>
2026-03-19 15:30:03 -0300

    Enquanto

---
+++ enquanto.js
@@ -0,0 +1,14 @@
+//Condição com while
+
+//let contador = 0
+
+//while(contador <= 5){console.log('Numero atual: ', contador); contador++ }
+
+
+//Condição com "do while"
+
+//let numero = 0
+
+//do {console.log('Numero atual: ', numero), numero++} while(numero <=10)
+
+
