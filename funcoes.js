576f51492599268649c9b3a17875a9a0495ad1d6
Joel-A-Queiroz <joel.andradequeiroz2018@gmail.com>
2026-03-20 12:17:12 -0300

    atualiza funçoes

---
+++ funcoes.js
@@ -0,0 +1,35 @@
+// function saudacao(nome) {
+// console.log('Olá', nome)
+// }
+
+//Arrow function
+const saudacao = (nome) => {
+    console.log('Vida longa a:', nome)
+}
+
+ saudacao('Joel!')
+ saudacao('Giselle')
+ saudacao('Geraldo')
+ saudacao('Valéria')
+ saudacao('Pedro')
+
+
+// function calcularDobro (numero){
+//     return numero * 2
+// }
+
+// const valorDobrado = calcularDobro(4)
+// console.log('O dobro de 4 é:', valorDobrado)
+
+
+//Primeiro teste de função:
+
+function testeDaVerdade (idade){
+    if(idade <18) {console.log('Não pode comprar bebida')}
+    else {console.log('Pode comprar bebida')}
+}
+
+testeDaVerdade(20)
+
+/////////////////////////////////////////////////////////////
+
