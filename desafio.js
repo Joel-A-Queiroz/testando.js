140749dd608b44323262be61c81b858334a57c5f
Joel-A-Queiroz <joel.andradequeiroz2018@gmail.com>
2026-03-23 19:36:05 -0300

    Desafio 2

--- desafio.js
+++ desafio.js
@@ -1,70 +1,76 @@
-//console.log('Boas vindas a calculadora de troco')
+// //console.log('Boas vindas a calculadora de troco')
 
-//let valorCompra = 17.50
-//let valorPago = 20
+// //let valorCompra = 17.50
+// //let valorPago = 20
 
-//console.log('Valor pago: R$ ', valorPago)
-//console.log('Valor compra: R$ ', valorCompra)
+// //console.log('Valor pago: R$ ', valorPago)
+// //console.log('Valor compra: R$ ', valorCompra)
 
-//let valorTroco = valorPago - valorCompra
+// //let valorTroco = valorPago - valorCompra
 
-//console.log('Valor do troco é: R$', valorTroco)
+// //console.log('Valor do troco é: R$', valorTroco)
 
-// Verificação de maioridade:
+// // Verificação de maioridade:
 
-let idade = 18
-let maiorDeIdade = idade >= 18
+// let idade = 18
+// let maiorDeIdade = idade >= 18
 
 
-console.log("É maior de idade? ", maiorDeIdade);
+// console.log("É maior de idade? ", maiorDeIdade);
 
-// Verificação de nota:
+// // Verificação de nota:
 
-let portugues = 10
-let matematica = 4
-let media = (portugues + matematica)/2
+// let portugues = 10
+// let matematica = 4
+// let media = (portugues + matematica)/2
 
-console.log('Média: ', media);
+// console.log('Média: ', media);
 
-let passouDeAno = media >=7
-console.log('Passou de ano? ', passouDeAno);
+// let passouDeAno = media >=7
+// console.log('Passou de ano? ', passouDeAno);
 
-//Simulação troco:
+// //Simulação troco:
 
-let valorCompra = 35.90
-let valorPago = 50
+// let valorCompra = 35.90
+// let valorPago = 50
 
-console.log('Valor compra:R$ ', valorCompra)
-console.log('Valor Pago: R$ ', valorPago)
+// console.log('Valor compra:R$ ', valorCompra)
+// console.log('Valor Pago: R$ ', valorPago)
 
-let valorTroco = valorPago - valorCompra
+// let valorTroco = valorPago - valorCompra
 
-console.log('Valor do troco é: R$ ', valorTroco)
+// console.log('Valor do troco é: R$ ', valorTroco)
 
-// Verificação de senha:
+// // Verificação de senha:
 
-const senhaPrimeiro = 1234
-const senhaSegundo = 1233
+// const senhaPrimeiro = 1234
+// const senhaSegundo = 1233
 
-console.log('A senha é igual?: ', senhaPrimeiro == senhaSegundo);
+// console.log('A senha é igual?: ', senhaPrimeiro == senhaSegundo);
 
-//Validação de faltas:
+// //Validação de faltas:
 
-let totalAulas = 100
-let totalFaltas = 24
+// let totalAulas = 100
+// let totalFaltas = 24
 
-let porcentagemDefaltas = totalFaltas / totalAulas *100
+// let porcentagemDefaltas = totalFaltas / totalAulas *100
 
-console.log('Porcentagem: ', porcentagemDefaltas)
+// console.log('Porcentagem: ', porcentagemDefaltas)
 
-let aprovado = porcentagemDefaltas < 25
+// let aprovado = porcentagemDefaltas < 25
 
-console.log('Aprovado?: ', aprovado);
+// console.log('Aprovado?: ', aprovado);
 
-//Verificação de login:
+// //Verificação de login:
 
-let temLogin = true
-let temSenha = true
+// let temLogin = true
+// let temSenha = true
 
-console.log('Tem login e senha? ', temLogin && temSenha);
+// console.log('Tem login e senha? ', temLogin && temSenha);
+
+
+
+//Desafio do terminal:
+
+
 
